4e2b232f852611d0d8fd6351a114762267db01a4
Dima Lazarev <dmitry.lazarev@gmail.com>
2020-12-24 20:08:19 +0200

    Add alternative solution for Day 1
    Using bool array instead of HashSet: first part is slightly slower this way, but part 2 is six times faster.

--- src/day1.rs
+++ src/day1.rs
@@ -22,6 +22,24 @@ fn part1(entries: &[Entry]) -> Option<Entry> {
     None
 }
 
+#[aoc(day1, part1, BoolArray)]
+fn part1_bool_array(entries: &[Entry]) -> Option<Entry> {
+    let dictionary: [bool; 2021] = entries.iter().fold([false; 2021], |mut acc, entry| {
+        acc[*entry as usize] = true;
+        acc
+    });
+
+    for first in entries {
+        let second = 2020 - first;
+
+        if second >= 0 && dictionary[second as usize] {
+            return Some(first * (2020 - first));
+        }
+    }
+
+    None
+}
+
 #[aoc(day1, part2)]
 fn part2(entries: &[Entry]) -> Option<Entry> {
     let dictionary: HashSet<_> = entries.iter().collect();
@@ -40,6 +58,28 @@ fn part2(entries: &[Entry]) -> Option<Entry> {
     None
 }
 
+#[aoc(day1, part2, BoolArray)]
+fn part2_bool_array(entries: &[Entry]) -> Option<Entry> {
+    let dictionary: [bool; 2021] = entries.iter().fold([false; 2021], |mut acc, entry| {
+        acc[*entry as usize] = true;
+        acc
+    });
+
+    for i in 0..entries.len() - 1 {
+        for j in i + 1..entries.len() {
+            let first = entries[i];
+            let second = entries[j];
+            let third = 2020 - (first + second);
+
+            if third >= 0 && dictionary[third as usize] {
+                return Some(first * second * third);
+            }
+        }
+    }
+
+    None
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -47,12 +87,20 @@ mod tests {
     #[test]
     fn part1_example() {
         assert_eq!(part1(&[1_721, 979, 366, 299, 675, 1_456]), Some(514_579));
+        assert_eq!(
+            part1_bool_array(&[1_721, 979, 366, 299, 675, 1_456]),
+            Some(514_579)
+        );
     }
 
     #[test]
     fn part2_example() {
         assert_eq!(
             part2(&[1_721, 979, 366, 299, 675, 1_456]),
+            Some(241_861_950)
+        );
+        assert_eq!(
+            part2_bool_array(&[1_721, 979, 366, 299, 675, 1_456]),
             Some(241_861_950)
         );
     }
